2a02e7d4d49d58db965899ca01bc7fbfbfb25f5d
jocsas <jocsa.santos@dotgroup.com.br>
2023-04-29 22:25:31 -0300

    add google search

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,10 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <meta
+          name="google-site-verification"
+          content="kNqHDkK4fVyqBNAMSmCbySYs5XYGfrzKd02s8UfTC90"
+        />
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
